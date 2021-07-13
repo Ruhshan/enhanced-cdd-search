552530924ccffec3cb1769ac1ece4a7ffdb8c1ee
Ruhshan <Ruhshan>
2021-07-14 00:09:32 +0600

    Add received status code from ncbi in message

--- backend/exception/custom_exception_handler.py
+++ backend/exception/custom_exception_handler.py
@@ -7,7 +7,7 @@ from backend.exception.custom_exceptions import CallToNCBIFailed
 def handle_call_to_NCBIFailed_exception(request: Request, exc: CallToNCBIFailed)->JSONResponse:
     return JSONResponse(
         status_code=418,
-        content={"message": "Unable to communicate with NCBI"},
+        content={"message": "Unable to communicate with NCBI, received status code "+exc.message},
     )
 
 
