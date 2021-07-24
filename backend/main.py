ce20c92859fcda34d3a81c5bf500cad16fdbae49
Ruhshan <Ruhshan>
2021-07-25 00:24:54 +0600

    Add endpoint for launching batch search

--- backend/main.py
+++ backend/main.py
@@ -2,8 +2,9 @@ from fastapi import FastAPI
 import uvicorn
 from starlette.middleware.cors import CORSMiddleware
 from backend.core.config import settings
-from backend.exception.custom_exception_handler import inclue_custom_exception_handler
+from backend.exception.custom_exception_handler import include_custom_exception_handler
 from backend.routers import search_router
+from backend.service.async_http_client import AIOClient
 
 app = FastAPI()
 
@@ -22,7 +23,15 @@ if settings.BACKEND_CORS_ORIGINS:
 
 
 app.include_router(search_router.router,prefix=settings.API_V1_STR+"/search")
-inclue_custom_exception_handler(app)
+include_custom_exception_handler(app)
+
+@app.on_event('startup')
+async def startup_event():
+    await AIOClient.create_session()
+
+@app.on_event('shutdown')
+async def shutdown_event():
+    await AIOClient.close_session()
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8090)
--- backend/routers/search_router.py
+++ backend/routers/search_router.py
@@ -2,9 +2,10 @@ from typing import List
 
 from fastapi import APIRouter
 
-from backend.schema.cd_entry import CdEntry
+from backend.schema.batch_search_schemas import BatchSearchResponse, BatchSearchRequest
 from backend.schema.search_request import SearchRequest
 from backend.schema.search_response import SearchResponse
+from backend.service.batch_cdd_scrapper import BatchCddScrapper
 from backend.service.search_handler import SearchHandler
 
 router = APIRouter()
@@ -13,3 +14,9 @@ router = APIRouter()
 @router.post("/new", response_model=SearchResponse)
 async def search(search_request: SearchRequest):
     return SearchHandler.new_search(search_request)
+
+
+@router.post("/batch", response_model=BatchSearchResponse)
+async def batch_search(search_request: BatchSearchRequest):
+    return await BatchCddScrapper.launch_search(search_request)
+
