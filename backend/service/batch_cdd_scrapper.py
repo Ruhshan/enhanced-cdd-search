49167bc5879f1fb12e82e0f10767096c2090a423
Ruhshan <Ruhshan>
2021-07-24 23:55:29 +0600

    Make BatchCddScrapper methods async

--- backend/service/batch_cdd_scrapper.py
+++ backend/service/batch_cdd_scrapper.py
@@ -1,65 +1,51 @@
-import requests
 import re
 
-from backend.exception.custom_exceptions import CallToNCBIFailed, InvalidBatchSearchId, JobIsStillRunning, \
+from backend.exception.custom_exceptions import InvalidBatchSearchId, JobIsStillRunning, \
     BatchSearchIdNotFound
 from backend.schema.batch_search_request import BatchSearchRequest, BatchSearchResponse
+from backend.service.async_http_client import AIOClient
 
 
 class BatchCddScrapper:
-    base_url="https://www.ncbi.nlm.nih.gov/Structure/bwrpsb/bwrpsb.cgi"
-    @classmethod
-    def launch_search(cls, request: BatchSearchRequest) ->BatchSearchResponse:
-        res = requests.request("POST", cls.base_url, data=request.dict())
+    base_url = "https://www.ncbi.nlm.nih.gov/Structure/bwrpsb/bwrpsb.cgi"
 
-        if res.status_code != 200:
-            raise CallToNCBIFailed(str(res.status_code))
-        search_id = re.search("QM3-qcdsearch-\w+", res.text)
+    @classmethod
+    async def launch_search_async(cls, batch_search_request: BatchSearchRequest):
+        response = await AIOClient.post(cls.base_url, batch_search_request.dict())
+        search_id = re.search("QM3-qcdsearch-\w+", response)
         if search_id:
             return BatchSearchResponse(search_id=search_id.group(0))
-
         raise BatchSearchIdNotFound("")
 
-
     @classmethod
-    def get_cdsid_from_search_id(cls, search_id) -> str:
-        res = requests.request("GET",cls.base_url+"?cdsid={}&tdata=aligns&alnfmt=text&dmode=all".format(
-                                   search_id))
-
-        if res.status_code != 200:
-            raise CallToNCBIFailed(str(res.status_code))
+    async def get_cdsid_from_search_id(cls, search_id) -> str:
+        response = await AIOClient.get(f'{cls.base_url}?cdsid={search_id}&tdata=aligns&alnfmt=text&dmode=all')
         try:
-            match_search_status = re.search("#status\s\d+", res.text)
+            match_search_status = re.search("#status\s\d+", response)
 
             search_status = re.split("\s", match_search_status.group(0))[-1]
 
             if search_status != "3":
                 raise InvalidBatchSearchId(search_id)
 
-            match_cdsid = re.search("#cdsid\s.*", res.text)
+            match_cdsid = re.search("#cdsid\s.*", response)
             return re.split("\s", match_cdsid.group(0))[-1]
         except Exception:
             raise InvalidBatchSearchId(search_id)
 
     @classmethod
-    def get_alignment_from_cds_id(cls, cds_id) -> str:
-        res = requests.request("GET",
-                               "https://www.ncbi.nlm.nih.gov/Structure/bwrpsb/bwrpsb.cgi?cdsid={}&tdata=aligns&alnfmt=text&dmode=all".format(
-                                   cds_id))
+    async def get_alignment_from_cds_id(cls, cds_id) -> str:
 
-        if res.status_code != 200:
-            raise CallToNCBIFailed(str(res.status_code))
+        response = await AIOClient.get(f'{cls.base_url}?cdsid={cds_id}&tdata=aligns&alnfmt=text&dmode=all')
 
-        job_is_running = re.search("msg\s+Job\s+is\s+still\s+running",res.text)
+        job_is_running = re.search("msg\s+Job\s+is\s+still\s+running", response)
 
         if job_is_running:
             raise JobIsStillRunning("-".join(cds_id.split("-")[:3]))
 
-        search_status_success = re.search("#status\ssuccess",res.text)
+        search_status_success = re.search("#status\ssuccess", response)
 
         if search_status_success is None:
             raise InvalidBatchSearchId(cds_id)
 
-        return res.text
-
-
+        return response
