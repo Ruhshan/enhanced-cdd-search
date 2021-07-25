364997c673c320d91634b709cddb576314f8ca35
Ruhshan <Ruhshan>
2021-07-25 11:01:13 +0600

    Update exception handling status codes

--- backend/exception/custom_exception_handler.py
+++ backend/exception/custom_exception_handler.py
@@ -7,14 +7,14 @@ from backend.exception.custom_exceptions import CallToNCBIFailed, DhandleNotFoun
 
 def handle_call_to_NCBIFailed_exception(request: Request, exc: CallToNCBIFailed) -> JSONResponse:
     return JSONResponse(
-        status_code=418,
+        status_code=500,
         content={"message": "Unable to communicate with NCBI, received status code " + exc.message},
     )
 
 
 def handle_dhandle_not_found_exception(request: Request, exc: DhandleNotFound) -> JSONResponse:
     return JSONResponse(
-        status_code=419,
+        status_code=500,
         content={"message": "Unable to parse data handle from ncbi response " + exc.message},
     )
 
@@ -27,11 +27,11 @@ def handle_job_is_still_running_exception(request: Request, exc: JobIsStillRunni
 
 
 def handle_invalid_batch_search_id(request: Request, exc: InvalidBatchSearchId) -> JSONResponse:
-    return JSONResponse(status_code=420, content={"message": "Invalid batch search id" + exc.message})
+    return JSONResponse(status_code=400, content={"message": "Invalid batch search id" + exc.message})
 
 
-def handle_batch_search_id_not_fount(request: Request, exc: BatchSearchIdNotFound) -> JSONResponse:
-    return JSONResponse(status_code=420,
+def handle_batch_search_id_not_found(request: Request, exc: BatchSearchIdNotFound) -> JSONResponse:
+    return JSONResponse(status_code=500,
                         content={"message": "Search Id not parseable"})
 
 
@@ -40,4 +40,4 @@ def include_custom_exception_handler(app: FastAPI):
     app.add_exception_handler(DhandleNotFound, handle_dhandle_not_found_exception)
     app.add_exception_handler(InvalidBatchSearchId, handle_invalid_batch_search_id)
     app.add_exception_handler(JobIsStillRunning, handle_job_is_still_running_exception)
-    app.add_exception_handler(BatchSearchIdNotFound, handle_batch_search_id_not_fount)
+    app.add_exception_handler(BatchSearchIdNotFound, handle_batch_search_id_not_found)
