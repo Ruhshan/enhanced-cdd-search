e617b4c11a670b59d8ecdc7e4c5ba657e0f26ddc
Ruhshan <Ruhshan>
2021-07-29 22:23:32 +0600

    Enable Filtering from result

--- frontend/src/components/ResultTable.vue
+++ frontend/src/components/ResultTable.vue
@@ -3,16 +3,34 @@
         <v-spacer style="height: 20px"></v-spacer>
         <v-card v-if="count > 0">
             <v-card-text>
-                <v-data-table :headers="headers" :items="results">
+                <v-data-table :headers="headers" :items="filteredResults">
                     <template v-slot:top>
                         <v-toolbar flat>
                             <v-toolbar-title
-                                >Retrieved {{ count }} CDs</v-toolbar-title
+                                >Retrieved {{ count }} CDs<span
+                                    v-if="filteredCds"
+                                    >| Filtered {{ filteredCds }} CDs</span
+                                ></v-toolbar-title
                             >
                             <v-spacer></v-spacer>
-                            <v-btn depressed color="info" @click="downloadFasta"
-                                >Download</v-btn
-                            >
+                            <v-col cols="3">
+                                <v-select
+                                    :items="dbPrefixes"
+                                    item-text="name"
+                                    item-value="value"
+                                    label="Filter Sources"
+                                    v-model="selectedDbPrefix"
+                                >
+                                    <template v-slot:append-outer>
+                                        <v-btn
+                                            depressed
+                                            color="info"
+                                            @click="downloadFasta"
+                                            >Download</v-btn
+                                        >
+                                    </template>
+                                </v-select>
+                            </v-col>
                         </v-toolbar>
                     </template>
                 </v-data-table>
@@ -38,9 +56,30 @@ export default class ResultTable extends Vue {
         { text: 'EValue', value: 'evalue' },
         { text: 'Description', value: 'description' },
     ]
+    private dbPrefixes = [
+        { name: 'None', value: '' },
+        { name: 'CD', value: 'cd' },
+        { name: 'SMART', value: 'smart' },
+        { name: 'PFAM', value: 'pfam' },
+    ]
+    private selectedDbPrefix = ''
+    private filteredCds = 0
 
     get count(): number {
         return this.results.length
+    }
+
+    get filteredResults(): Array<BatchCdEntry> {
+        if (this.selectedDbPrefix) {
+            var filteredResults = this.results.filter(
+                r => r.accession.indexOf(this.selectedDbPrefix) == 0
+            )
+            this.filteredCds = filteredResults.length
+            return filteredResults
+        } else {
+            this.filteredCds = 0
+            return this.results
+        }
     }
 
     private async downloadFasta(): Promise<void> {
